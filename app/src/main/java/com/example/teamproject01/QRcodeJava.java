ad6c3c09c3eb3afa555f251ca7583ffe765a8790
Profrog <ache159@naver.com>
2020-10-01 01:03:08 +0900

    first added

--- app/src/main/java/com/example/teamproject01/QRcodeJava.java
+++ app/src/main/java/com/example/teamproject01/QRcodeJava.java
@@ -1,13 +1,35 @@
 package com.example.teamproject01;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 public class QRcodeJava extends AppCompatActivity {
+
+    private ImageView iv;
+    private String text;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qrcodejava);
+
+
+        iv = (ImageView)findViewById(R.id.qrcode);
+        text = ((MainActivity)MainActivity.forstatic).getReturnString();
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            iv.setImageBitmap(bitmap);
+        }catch (Exception e){}
     }
 }
--- app/src/main/java/com/example/teamproject01/purchasingJava.java
+++ app/src/main/java/com/example/teamproject01/purchasingJava.java
@@ -1,13 +1,74 @@
 package com.example.teamproject01;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.microedition.khronos.egl.EGLDisplay;
 
 public class purchasingJava extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.purchasingjava);
+    }
+
+    private AlertDialog.Builder builder;
+    private DialogInterface.OnClickListener checking = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            Date currentTime = Calendar.getInstance().getTime();
+            SimpleDateFormat year = new SimpleDateFormat("yyyy", Locale.getDefault());
+            SimpleDateFormat month = new SimpleDateFormat("MM", Locale.getDefault());
+
+            String year1 = year.format((currentTime));
+            String month1 = month.format((currentTime));
+
+            if(Integer.parseInt(month1) > 6)
+                month1 = "1";
+            else
+                month1 = "2";
+
+            ((MainActivity)MainActivity.forstatic).changingDateenroll(year1 + month1);
+        }
+
+        public void onClick() {
+            // do something when the button is clicked
+            Date currentTime = Calendar.getInstance().getTime();
+            SimpleDateFormat year = new SimpleDateFormat("yyyy", Locale.getDefault());
+            SimpleDateFormat month = new SimpleDateFormat("MM", Locale.getDefault());
+
+            String year1 = year.format((currentTime));
+            String month1 = month.format((currentTime));
+
+            if(Integer.parseInt(month1) > 6)
+                month1 = "1";
+            else
+                month1 = "2";
+
+            ((MainActivity)MainActivity.forstatic).changingDateenroll(year1 + month1);
+        }
+    };
+
+
+    public void clicksaving1(View v){
+        builder= new AlertDialog.Builder((purchasingJava.this));
+        builder.setTitle("결제창");
+        EditText enroll = (EditText) findViewById(R.id.enroll_date);
+
+        builder.setMessage(enroll.getText().toString() + "을 결제하시겠습니까?");
+        builder.setPositiveButton("확인", checking);
+        builder.setNegativeButton("취소",null);
+        builder.create().show();
     }
 }
